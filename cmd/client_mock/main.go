b0bb9eb9ca40271fbacb37658317c1d51d332a7f
MotyaSS <motyazz32@gmail.com>
2026-03-09 18:39:05 +0300

    mock client is now configured by scrapper config for consistency

--- cmd/client_mock/main.go
+++ cmd/client_mock/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/MotyaSS/IoTMonitoring/internal/config"
 	IoTMonitoring "github.com/MotyaSS/IoTMonitoring/internal/scrapper/gen"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,8 +17,18 @@ const reqCount = 5
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	cfg, err := config.Load("configs/scrapper.yaml")
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
 
-	conn, err := grpc.NewClient(":8080",
+	if cfg.GRPC == nil {
+		fmt.Println("No gRPC configuration")
+		return
+	}
+
+	conn, err := grpc.NewClient(cfg.GRPC.Addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
