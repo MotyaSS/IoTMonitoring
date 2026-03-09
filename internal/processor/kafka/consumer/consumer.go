95f420ba1624a08f013386d2564036fe7dbcfafd
MotyaSS <motyazz32@gmail.com>
2026-03-09 18:41:11 +0300

    implemented consumer and producer for processor

--- internal/processor/kafka/consumer/consumer.go
+++ internal/processor/kafka/consumer/consumer.go
@@ -1,1 +1,33 @@
 package consumer
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/MotyaSS/IoTMonitoring/internal/config"
+	"github.com/segmentio/kafka-go"
+)
+
+type ProcessorConsumer struct {
+	r   *kafka.Reader
+	log *slog.Logger
+}
+
+func NewProcessorConsumer(cfg *config.KafkaConfig, log *slog.Logger) *ProcessorConsumer {
+	return &ProcessorConsumer{
+		r: kafka.NewReader(kafka.ReaderConfig{
+			Brokers: cfg.Brokers,
+			GroupID: cfg.GroupID,
+			Topic:   *cfg.InputTopic,
+		}),
+		log: log,
+	}
+}
+
+func (p *ProcessorConsumer) Consume(ctx context.Context) (kafka.Message, error) {
+	return p.r.ReadMessage(ctx)
+}
+
+func (p *ProcessorConsumer) Close() error {
+	return p.r.Close()
+}
--- internal/processor/kafka/producer/producer.go
+++ internal/processor/kafka/producer/producer.go
@@ -1,1 +1,56 @@
 package producer
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"time"
+
+	"github.com/MotyaSS/IoTMonitoring/internal/config"
+	"github.com/MotyaSS/IoTMonitoring/internal/types"
+	"github.com/segmentio/kafka-go"
+)
+
+type ProcessorProducer struct {
+	w   *kafka.Writer
+	log *slog.Logger
+}
+
+func NewScrapperProducer(cfg *config.KafkaConfig, log *slog.Logger) (*ProcessorProducer, error) {
+	if cfg.OutputTopic == nil {
+		return nil, errors.New("output topic required")
+	}
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP(cfg.Brokers...),
+		Topic:                  *cfg.OutputTopic,
+		WriteTimeout:           10 * time.Second,
+		AllowAutoTopicCreation: true,
+		Balancer:               &kafka.RoundRobin{},
+	}
+	return &ProcessorProducer{
+		w:   w,
+		log: log,
+	}, nil
+}
+
+func (kp *ProcessorProducer) Produce(ctx context.Context, msg types.ProcessedData) error {
+	v, err := json.Marshal(msg)
+	if err != nil {
+		kp.log.Error("Failed to marshal telemetry to json", "error", err)
+		return err
+	}
+	kp.log.Debug("Sending telemetry to kafka", "payload", string(v))
+	return kp.w.WriteMessages(
+		ctx,
+		kafka.Message{
+			Key:   nil,
+			Value: v,
+		},
+	)
+
+}
+
+func (kp *ProcessorProducer) Close() error {
+	return kp.w.Close()
+}
